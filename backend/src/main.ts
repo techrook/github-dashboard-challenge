349a76914c56ea02d7ba0f7421ea4c6ced839750
techrook <mondayitohowo3@gmail.com>
2026-07-17 14:44:39 +0100

    FEAT: added swagger for documentation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,7 @@ import { ConfigService } from '@nestjs/config';
 import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder,SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -22,7 +23,19 @@ async function bootstrap() {
   app.useGlobalInterceptors(
     new ResponseInterceptor(),
   );
-  
+  const config = new DocumentBuilder()
+  .setTitle('GitHub Developer Dashboard API')
+  .setDescription(
+    'Backend API for fetching GitHub user profiles, repositories, languages, statistics, and recent public activity.',
+  )
+  .setVersion('1.0.0')
+  .addTag('GitHub')
+  .build();
+
+const document = SwaggerModule.createDocument(app, config);
+
+SwaggerModule.setup('docs', app, document);
+
   const configService = app.get<ConfigService>(ConfigService);
   const port = process.env.PORT || configService.get('port');
   await app.listen(port);
